9c1598bb546570e4d9a2d62176c19252e5cdaf4d
Gaurav <gauravkherde145@gmail.com>
2022-04-07 23:36:57 +0530

    create and edit ui done

--- app/src/main/java/com/gaurav/notesapp/ui/Fragments/CreateNotesFragment.kt
+++ app/src/main/java/com/gaurav/notesapp/ui/Fragments/CreateNotesFragment.kt
@@ -6,16 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.gaurav.notesapp.R
+import com.gaurav.notesapp.databinding.FragmentCreateNotesBinding
 
 
 class CreateNotesFragment : Fragment() {
 
+    lateinit var binding:FragmentCreateNotesBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_notes, container, false)
+        binding= FragmentCreateNotesBinding.inflate(layoutInflater,container,false)
+
+        return binding.root
     }
 
 
--- app/src/main/java/com/gaurav/notesapp/ui/Fragments/HomeFragment.kt
+++ app/src/main/java/com/gaurav/notesapp/ui/Fragments/HomeFragment.kt
@@ -5,18 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.gaurav.notesapp.R
+import com.gaurav.notesapp.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding= FragmentHomeBinding.inflate(layoutInflater,container,false)
+        binding.btnAddNotes.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_createNotesFragment)
+        }
+        return binding.root
     }
 
 
